ce8894780e38635736d9597b59f9fecea1afb8c7
Niharika Thakur <70130153+niharikaathakur@users.noreply.github.com>
2024-07-19 19:22:47 +0530

    Time: 2 ms (10.06%) | Memory: 45.9 MB (16.88%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,27 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
+        int start = 0;
+        int end = nums.length-1;
+        int mid = start+(end-start)/2;
 
-        while (low <= high) {
-            // Finding the mid using integer division
-            int mid = low + (high - low) / 2;
-
-            // Target value is present at the middle of the array
-            if (nums[mid] == target) {
-                return mid;
+        while(start<=end){
+            mid = start+end-start/2;
+            if(nums[mid]==target){
+                return mid; 
             }
-            // Target value is present in the low subarray
-            else if (target < nums[mid]) {
-                high = mid - 1;
+            else if(target>nums[mid]){
+                start = mid+1;
             }
-            // Target value is present in the high subarray
-            else if (target > nums[mid]) {
-                low = mid + 1;
-            }
+            else if(target<nums[mid]){
+                end = mid-1;
+            } 
         }
-
-        // Target value is not present in the array
         return -1;
     }
 }
